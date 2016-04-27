cf8297eaf388ec09fad406c27726e3de1eb50830
Brandon Dorman <brandon@opened.com>
2016-04-27 14:02:00 -0700

    first upload

---
+++ brandongettest.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("http://api.commonstandardsproject.com/api/v1/standard_sets/49FCDFBD2CF04033A9C347BFA0584DF0_D2604890_grade-01?api-key=pkGLztAUWgJHZv5YbpHoDsV6")
+	if err != nil {
+		log.Fatal(err)
+	}
+	page, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", page)
+}
