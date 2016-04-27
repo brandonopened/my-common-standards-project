3dd1d6d1c4808146c8957288a7ffc7d575385d73
Ron Drabkin <ron@opened.com>
2016-04-27 15:30:50 -0700

    Update brandongettest.go
    trying the other package

--- brandongettest.go
+++ brandongettest.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"encoding/csv"
 	"log"
 	"net/http"
 )
+
+var data = [][]string{{"Line1", "Grade"}, {"Line2", "Resource"}}
 
 func main() {
 	res, err := http.Get("http://api.commonstandardsproject.com/api/v1/standard_sets/49FCDFBD2CF04033A9C347BFA0584DF0_D2604890_grade-01?api-key=pkGLztAUWgJHZv5YbpHoDsV6")
 	if err != nil {
 		log.Fatal(err)
-	}
-	page, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-		fmt.Printf("%s", page)
+         
+        file, err := os.Create("result.csv")
+    checkError("Cannot create file", err)
+    defer file.Close()
+
+    writer := csv.NewWriter(file)
+
+    for _, value := range data {
+        err := writer.Write(value)
+        checkError("Cannot write to file", err) 
+
 }
